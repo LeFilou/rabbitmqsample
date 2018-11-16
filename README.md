2367609076d7fb564f85bd5167e14c1136323aeb
lefilou <fliousalim@gmail.com>
2018-11-16 22:58:52 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# RabbitMq Sample
+> A simple spring boot app that uses Rabbitmq
+
+## Getting started
+
+Run Rabbitmq on docker:
+
+```shell
+docker container run -p 15672:15672 rabbitmq
+```
---
+++ src/main/java/com/rabbitmq/rabbitmqsample/RabbitmqsampleApplication.java
@@ -0,0 +1,12 @@
+package com.rabbitmq.rabbitmqsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqsampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqsampleApplication.class, args);
+	}
+}
